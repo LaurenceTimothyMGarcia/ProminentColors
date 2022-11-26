7dfc54135e96b1ddfa4f5364377648a1815a5de2
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2022-11-25 18:12:01 -0800

    initalized buttons and added comments

--- app/src/main/java/com/cpp/android/prominentcolors/MainActivity.kt
+++ app/src/main/java/com/cpp/android/prominentcolors/MainActivity.kt
@@ -2,10 +2,31 @@ package com.cpp.android.prominentcolors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+
+    private lateinit var selectImageButton: Button
+    private lateinit var useCameraButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initializes buttons
+        selectImageButton = findViewById(R.id.select_image)
+        useCameraButton = findViewById(R.id.camera)
+
+        selectImageButton.setOnClickListener()
+        {
+            //Open image gallery
+        }
+
+        useCameraButton.setOnClickListener()
+        {
+            //Open camera app
+        }
     }
 }
