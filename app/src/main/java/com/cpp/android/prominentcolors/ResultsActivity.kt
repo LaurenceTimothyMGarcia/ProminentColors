6125396c20ce7fe181b80d3758fb469469148102
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-12-08 16:34:44 -0800

    displays mutliple colors

--- app/src/main/java/com/cpp/android/prominentcolors/ResultsActivity.kt
+++ app/src/main/java/com/cpp/android/prominentcolors/ResultsActivity.kt
@@ -16,6 +16,8 @@ class ResultsActivity : AppCompatActivity()
 {
     //UI Elements
     //Most prominent color
+    //Uses shows rank of color
+    private lateinit var colorRank: TextView
     //use a white square and change its rgb value
     private lateinit var mostUsedColor: ImageView
     //Hexcode Text
@@ -23,7 +25,10 @@ class ResultsActivity : AppCompatActivity()
     //HexCode Count (mostly for debugging)
     //Maybe change to percentage later on
     private lateinit var hexCodeCount: TextView
+
     //Buttons
+    private lateinit var colorRankBack: Button
+    private lateinit var colorRankFoward: Button
     private lateinit var copyToClipboard: Button
     private lateinit var useAppAgain: Button
 
@@ -31,6 +36,7 @@ class ResultsActivity : AppCompatActivity()
     private lateinit var hexColorString: Array<String>
     private lateinit var hexColorValue: IntArray
     private lateinit var hexColorCount: IntArray
+    private var hexCodeRank: Int = 0
 
     private val TAG = "ResultsActivity"
 
@@ -42,11 +48,14 @@ class ResultsActivity : AppCompatActivity()
         setContentView(R.layout.results)
 
         //Sync UI var to UI elements
+        colorRank = findViewById(R.id.color_rank)
         mostUsedColor = findViewById(R.id.color_image)
         hexCodeText = findViewById(R.id.color_hexcode)
         copyToClipboard = findViewById(R.id.copy_hexcode)
         useAppAgain = findViewById(R.id.use_again)
         hexCodeCount = findViewById(R.id.color_count)
+        colorRankBack = findViewById(R.id.back_rank_button)
+        colorRankFoward = findViewById(R.id.forward_rank_button)
 
 
         //GETS VALUES FROM BUNDLES
@@ -54,22 +63,56 @@ class ResultsActivity : AppCompatActivity()
         hexColorValue = intent.getIntArrayExtra("HexCodeValue")!!
         hexColorCount = intent.getIntArrayExtra("HexCodeCount")!!
 
+        //Create textview to show rank of color
+        colorRank.text = (hexCodeRank + 1).toString()
 
         //Gets hexcode text from bundle in loading
-        hexCodeText.text = hexColorString[0]
+        hexCodeText.text = hexColorString[hexCodeRank]
 
         //Set mostUsedColor to white box
         //Initialize white box to hexcode value
         mostUsedColor.setImageResource(R.drawable.color_hex)
         //Set argb value
-        mostUsedColor.setColorFilter(hexColorValue[0])
+        mostUsedColor.setColorFilter(hexColorValue[hexCodeRank])
 
         //Shows Count
-        hexCodeCount.text = "Count: ${hexColorCount[0].toString()}"
+        hexCodeCount.text = "Count: ${hexColorCount[hexCodeRank].toString()}"
 
 
         //Create buttons on left and right to rotate side of array
-        //Create textview to show "placement" of array
+        //Moves up rank
+        colorRankBack.setOnClickListener()
+        {
+            hexCodeRank--
+
+            if (hexCodeRank < 0)
+            {
+                hexCodeRank = hexColorString.size - 1
+            }
+
+            //Set all variables
+            colorRank.text = (hexCodeRank + 1).toString()
+            hexCodeText.text = hexColorString[hexCodeRank]
+            mostUsedColor.setColorFilter(hexColorValue[hexCodeRank])
+            hexCodeCount.text = "Count: ${hexColorCount[hexCodeRank].toString()}"
+        }
+
+        //Moves down rank
+        colorRankFoward.setOnClickListener()
+        {
+            hexCodeRank++
+
+            if (hexCodeRank >= hexColorString.size)
+            {
+                hexCodeRank = 0
+            }
+
+            //Set all variables
+            colorRank.text = (hexCodeRank + 1).toString()
+            hexCodeText.text = hexColorString[hexCodeRank]
+            mostUsedColor.setColorFilter(hexColorValue[hexCodeRank])
+            hexCodeCount.text = "Count: ${hexColorCount[hexCodeRank].toString()}"
+        }
 
         //If clicks button, copy hexcode to clipboard
         copyToClipboard.setOnClickListener()
